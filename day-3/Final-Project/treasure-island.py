347c3a35a4b5f08ea177caba328990e167727a6d
Anibal H <robhedz12@protonmail.com>
2021-11-28 15:25:55 -0600

    Finalizado proyecto dia 3

---
+++ day-3/Final-Project/treasure-island.py
@@ -0,0 +1,59 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+
+# Text Snippets
+# 'You\'re at a crossroad. Where do you want to go? Type "left" or "right"'
+# 'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.'
+# "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?"
+# "It's a room full of fire. Game Over."
+# "You found the treasure! You Win!"
+# "You enter a room of beasts. Game Over."
+# "You chose a door that doesn't exist. Game Over."
+# "You get attacked by an angry trout. Game Over."
+# "You fell into a hole. Game Over."
+
+direction = input("'You\'re at a crossroad. Where do you want to go? Type \"left\" or \"right\"'\n")
+
+if direction == "left":
+    swim_wait_answer = input("You\'ve come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n")
+    if swim_wait_answer == "wait":
+        door_selection = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+        if door_selection == "red":
+            print("It's a room full of fire. Game Over.\n")
+        elif door_selection == "yellow":
+            print("You found the treasure! You Win!\n")
+        elif door_selection == "blue":
+            print("You enter a room of beasts. Game Over.\n")
+        else:
+            print("Game Over\n")
+    else:
+        print("You get attacked by an angry trout. Game Over.\n")
+else:
+    print("You fell into a hole. Game Over.\n")
