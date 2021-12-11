c8746f486ddab0d08b88dc0a6607bea9dbc5ad85
Anibal H <robhedz12@protonmail.com>
2021-12-11 12:06:25 -0600

    day 9 - finished exercise 1

---
+++ day-9/Exercise-1/Exercise-1.py
@@ -0,0 +1,36 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+for student in student_scores:
+    if student_scores[student] >= 91:
+        # print("Outstanding")
+        student_grades[student] = "Outstanding"
+    elif student_scores[student] >= 81:
+        # print("Exceeds Expectations")
+        student_grades[student] = "Exceeds Expectations"
+    elif student_scores[student] >= 71:
+        # print("Acceptable")
+        student_grades[student] = "Acceptable"
+    else:
+        # print("Fail")
+        student_grades[student] = "Fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+
+
+
+
